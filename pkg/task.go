67b90ee0fbf626ef32cd6ea0ed6aaf81c367302b
CodingSoldier <tfz9011@163.com>
2022-05-15 14:16:12 +0800

    task

--- pkg/task.go
+++ pkg/task.go
@@ -6,31 +6,46 @@ import (
 	"time"
 )
 
-// TODO 修改task
-var entryID = 0
+type Task struct {
+	Cron    *cron.Cron
+	EntryID cron.EntryID
+}
 
-func InitTask() (cron.EntryID, error) {
+func (t *Task) InitTask() error {
 	// 读取配置文件
-	conf, err := GetConfig()
+	config, err := GetConfig()
 	if err != nil {
-		fmt.Printf("\nconfig.yaml配置信息为 %+v\n", conf)
-		return 0, err
+		fmt.Printf("\nconfig.yaml配置信息为 %+v\n", config)
+		return err
 	}
-	task := cron.New(cron.WithSeconds())
-	entryID, err := task.AddFunc(conf.CronTime, func() {
-		msg := conf.MessageContent + conf.MessagePassword
+	wxCron := cron.New(cron.WithSeconds())
+	entryID, err := wxCron.AddFunc(config.CronTime, func() {
+		msg := config.MessageContent + config.MessagePassword
 		fmt.Println(time.Now(), "发送企业微信消息，内容是 ", msg)
-		WxSend(conf)
+		WxSend(config)
 	})
 	if err != nil {
 		fmt.Println("创建定时任务出错了，错误信息是 ", err.Error())
-		return entryID, err
+		return err
 	}
 	fmt.Println("定时任务创建成功")
-	task.Start()
-	return entryID, nil
+	wxCron.Start()
+	t.EntryID = entryID
+	t.Cron = wxCron
+	return nil
 }
 
-func ChangeTask(config *Config) {
-
+func (t *Task) ChangeTask(config *Config) error {
+	entryID, err := t.Cron.AddFunc(config.CronTime, func() {
+		msg := config.MessageContent + config.MessagePassword
+		fmt.Println(time.Now(), "发送企业微信消息，内容是 ", msg)
+		WxSend(config)
+	})
+	if err != nil {
+		fmt.Println("创建定时任务出错了，错误信息是 ", err.Error())
+		return err
+	}
+	t.Cron.Remove(t.EntryID)
+	t.Cron.Entry(entryID)
+	return nil
 }
